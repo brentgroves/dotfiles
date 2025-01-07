e0b8d69c359fea96145528dca8b70d752997a1c0
brentgroves <brent.groves@gmail.com>
2025-01-07 13:30:17 -0500

    added codd

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -9,8 +9,10 @@ export PATH="$HOME/fpc-3.2.0/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="/home/brent/miniconda3/bin/conda:$PATH"
 # export PATH="/home/bgroves@BUSCHE-CNC.COM/anaconda3/bin:$PATH"
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH=/usr/local/go/bin:$HOME/go/bin/:$PATH
+export PATH="$HOME/bin:/usr/local/bin:$PATH"
+# go1.23.4 is at /usr/local/go/bin
+# export PATH="/usr/local/go/bin:$HOME/go/bin/:$PATH"
+export PATH="$HOME/sdk/go1.22.0:$HOME/go/bin/:$PATH"
 
 # export PATH=$HOME/.local/bin:$HOME/sdk/go1.22.0/bin/:$PATH
 export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}";
